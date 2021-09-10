b5168490db64cba1dd88aede9cc822064a8416b7
comye1 <yewon2664@gmail.com>
2021-09-10 21:40:17 +0900

    composable - DeckItem / gradle - MaterialIcons

--- app/src/main/java/com/comye1/Flashcards/MainActivity.kt
+++ app/src/main/java/com/comye1/Flashcards/MainActivity.kt
@@ -3,11 +3,20 @@ package com.comye1.Flashcards
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.TurnedIn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.comye1.Flashcards.ui.theme.FlashcardsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +26,16 @@ class MainActivity : ComponentActivity() {
             FlashcardsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Column {
+                        DeckItem()
+                        DeckItem()
+                        DeckItem()
+                        DeckItem()
+                        DeckItem()
+                        DeckItem()
+                        DeckItem()
+
+                    }
                 }
             }
         }
@@ -25,14 +43,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DeckItem() {
+    // Home 화면 내 & 각 Subject 화면 내
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(2.dp, Color.LightGray)
+            .padding(16.dp),
+    ) {
+        Text(
+            text = "c-plus-plus",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+//                text = "12 Decks · 207 Cards",
+                text = "11 Cards",
+                style = MaterialTheme.typography.subtitle1,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray
+            )
+            Icon(
+                imageVector = Icons.Default.TurnedIn,
+                contentDescription = "bookmarked",
+                tint = Color.Gray
+            )
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    FlashcardsTheme {
-        Greeting("Android")
-    }
+    DeckItem()
 }
