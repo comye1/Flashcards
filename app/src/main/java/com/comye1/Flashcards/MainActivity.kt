84b624cead08440fe21d141ec3d09b44f5067476
comye1 <yewon2664@gmail.com>
2021-09-11 13:48:47 +0900

    composable - MakeMyDeck, SubjectItem

--- app/src/main/java/com/comye1/Flashcards/MainActivity.kt
+++ app/src/main/java/com/comye1/Flashcards/MainActivity.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.TurnedIn
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.comye1.Flashcards.ui.theme.DeepOrange
 import com.comye1.Flashcards.ui.theme.FlashcardsTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +32,13 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Column {
+                        FindFlashCards()
                         DeckItem()
                         DeckInSubject()
                         StudyGuide()
+                        SubjectItem()
                         MyDeckItem()
+                        MakeMyDeck()
                     }
                 }
             }
@@ -45,8 +51,11 @@ fun DeckItem() {
     // Home 화면 내
     Column(
         modifier = Modifier
-            .padding(8.dp)
             .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+
+            }
             .border(2.dp, Color.LightGray)
             .padding(16.dp),
     ) {
@@ -80,8 +89,11 @@ fun MyDeckItem() {
     // Home 화면 내
     Column(
         modifier = Modifier
-            .padding(8.dp)
             .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+
+            }
             .border(2.dp, Color.LightGray)
             .padding(16.dp),
     ) {
@@ -115,14 +127,64 @@ fun MyDeckItem() {
     }
 }
 
+@Composable
+fun MakeMyDeck() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+
+            }
+            .border(2.dp, Color.LightGray)
+            .padding(20.dp),
+    ) {
+        Text(
+            text = "Make your own cards",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "It's easy to create your own flashcard deck -for free.",
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+//            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+//            IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Default.NoteAdd,
+                contentDescription = "add_deck",
+                tint = Color.Blue
+            )
+//            } IconButton 인 줄 알았더니 아무거나 클릭해도 되는거였어!!
+//            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = " Get started",
+                style = MaterialTheme.typography.subtitle1,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+            )
+        }
+    }
+}
+
 
 @Composable
 fun DeckInSubject() {
     // 각 Subject 화면 내
     Column(
         modifier = Modifier
-            .padding(8.dp)
             .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+
+            }
             .border(2.dp, Color.LightGray)
             .padding(16.dp),
     ) {
@@ -146,8 +208,11 @@ fun StudyGuide() {
     // 각 Subject 화면 내
     Column(
         modifier = Modifier
-            .padding(8.dp)
             .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+
+            }
             .border(2.dp, Color.LightGray)
             .padding(16.dp),
     ) {
@@ -163,6 +228,48 @@ fun StudyGuide() {
             fontWeight = FontWeight.Bold,
             color = Color.Gray
         )
+    }
+}
+
+@Composable
+fun SubjectItem() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(2.dp, Color.LightGray, shape = RoundedCornerShape(size = 8.dp))
+            .clickable {
+
+            }
+            .padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.Computer,
+            contentDescription = "computer science",
+            tint = DeepOrange,
+            modifier = Modifier.size(36.dp)
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            text = "Computer Science",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.ExtraBold
+        )
+    }
+}
+
+@Composable
+fun FindFlashCards() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 1.dp, color = Color.LightGray, shape = CircleShape)
+            .padding(horizontal = 8.dp, vertical = 14.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(imageVector = Icons.Default.Search, contentDescription = "search flashcards", tint = Color.Gray)
+        Text(text = " Find flashcards", style = MaterialTheme.typography.body1, color = Color.Gray)
     }
 }
 
