5c08133393504207db48cfec147f0333f1dad684
comye1 <yewon2664@gmail.com>
2021-09-11 18:59:38 +0900

    composable - FilterText, DeckTitleTextField, CardItem

--- app/src/main/java/com/comye1/Flashcards/MainActivity.kt
+++ app/src/main/java/com/comye1/Flashcards/MainActivity.kt
@@ -3,20 +3,19 @@ package com.comye1.Flashcards
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,11 +33,19 @@ class MainActivity : ComponentActivity() {
                     Column {
                         FindFlashCards()
                         DeckItem()
-                        DeckInSubject()
-                        StudyGuide()
-                        SubjectItem()
-                        MyDeckItem()
-                        MakeMyDeck()
+//                        DeckInSubject()
+//                        StudyGuide()
+//                        SubjectItem()
+//                        MyDeckItem()
+//                        MakeMyDeck()
+                        Row() {
+                            FilterText()
+                            FilterText()
+                            FilterText()
+                            FilterText()
+                        }
+                        DeckTitleTextField()
+                        CardItem()
                     }
                 }
             }
@@ -268,16 +275,93 @@ fun FindFlashCards() {
             .padding(horizontal = 8.dp, vertical = 14.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(imageVector = Icons.Default.Search, contentDescription = "search flashcards", tint = Color.Gray)
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = "search flashcards",
+            tint = Color.Gray
+        )
         Text(text = " Find flashcards", style = MaterialTheme.typography.body1, color = Color.Gray)
+    }
+}
+
+@Composable
+fun FilterText() {
+    Column(modifier = Modifier
+        .padding(4.dp)
+        .clip(shape = CircleShape)
+        .clickable(enabled = false) { }
+        .background(color = Color.LightGray)
+        .padding(horizontal = 20.dp, vertical = 4.dp)
+    ) {
+        Text("All", style = MaterialTheme.typography.body1, fontWeight = FontWeight.ExtraBold)
+    }
+}
+
+@Composable
+fun DeckTitleTextField() {
+    OutlinedTextField(
+        value = "",
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        textStyle = MaterialTheme.typography.h4,
+        placeholder = {
+            Text(
+                text = "Untitled deck",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.LightGray,
+                modifier = Modifier.padding(4.dp)
+            )
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Transparent,
+            unfocusedBorderColor = Color.Transparent,
+            cursorColor = DeepOrange,
+            backgroundColor = Color.Transparent
+        ),
+        maxLines = 2
+    )
+}
+
+@Composable
+fun CardItem() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(2.dp, Color.LightGray),
+    ) {
+        Text(
+            "Operating Systems",
+            modifier = Modifier.padding(16.dp),
+            fontWeight = FontWeight.ExtraBold
+        )
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(2.dp),
+            color = Color.LightGray
+        )
+        Text(
+            "A request to execute an OS service-layer function",
+            modifier = Modifier.padding(16.dp),
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Column() {
-        DeckItem()
-        DeckInSubject()
+    Column {
+//        FindFlashCards()
+//        DeckItem()
+//        DeckInSubject()
+//        StudyGuide()
+//        SubjectItem()
+//        MyDeckItem()
+//        MakeMyDeck()
+        CardItem()
     }
 }
