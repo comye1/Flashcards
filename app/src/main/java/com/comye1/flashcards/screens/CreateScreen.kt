2a5ec1d0691cb3177844ded031555bb4d347eb24
comye1 <comye1@seoultech.ac.kr>
2021-10-03 21:45:30 +0900

    CreateScreen - deckTitle hoisting, TopAppBar

--- app/src/main/java/com/comye1/flashcards/screens/CreateScreen.kt
+++ app/src/main/java/com/comye1/flashcards/screens/CreateScreen.kt
@@ -2,6 +2,8 @@ package com.comye1.flashcards.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,34 +13,83 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.comye1.flashcards.DeckTitleTextField
+import com.comye1.flashcards.ui.theme.DeepOrange
+import com.comye1.flashcards.ui.theme.LightOrange
 
 @Composable
 fun CreateScreen() {
 
-    val canGoNext by remember {
-        mutableStateOf(false)
+    val (deckTitle, setDectTitle) = remember {
+        mutableStateOf("")
+    }
+
+    val (visible, setVisibility) = remember {
+        mutableStateOf(true)
     }
 
     Scaffold(topBar = {
         TopAppBar(
             title = {
-                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                    Text("Create new deck", style = MaterialTheme.typography.h5, fontWeight = FontWeight.Bold)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        "Create new deck",
+                        style = MaterialTheme.typography.h5,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            },
+            navigationIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Outlined.Close,
+                        contentDescription = "close screen"
+                    )
                 }
             },
             backgroundColor = Color.Transparent,
             elevation = 0.dp,
             actions = {
                 // RowScope here, so these icons will be placed horizontally
-                TextButton(onClick = { /*TODO*/ }, enabled = canGoNext ) {
+                TextButton(onClick = { /*TODO*/ }, enabled = !deckTitle.isNullOrEmpty()) {
                     Text("Next", style = MaterialTheme.typography.h6, fontWeight = FontWeight.Bold)
                 }
             }
         )
     }
     ) {
-        Column (modifier = Modifier.fillMaxSize().padding(16.dp), verticalArrangement = Arrangement.Center){
-            DeckTitleTextField()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(.5f)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            DeckTitleTextField(deckTitle, setDectTitle)
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    "Visible to everyone",
+                    style = MaterialTheme.typography.h5,
+                    fontWeight = FontWeight.Bold
+                )
+                Switch(
+                    checked = visible,
+                    onCheckedChange = setVisibility,
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = DeepOrange,
+                        checkedTrackColor = LightOrange
+                    )
+                )
+            }
+            Text(
+                "Other Students can find, view, and study\nthis deck"
+            )
         }
     }
 }
