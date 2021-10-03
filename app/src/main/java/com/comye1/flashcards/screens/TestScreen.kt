07fd0288c15f0d66a77510d4fa9f4bc738c089b6
comye1 <comye1@seoultech.ac.kr>
2021-10-04 01:12:38 +0900

    TestScreen - 학습 진행상황 바

---
+++ app/src/main/java/com/comye1/flashcards/screens/TestScreen.kt
@@ -0,0 +1,90 @@
+package com.comye1.flashcards.screens
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.comye1.flashcards.ui.theme.DeepOrange
+
+@Preview
+@Composable
+fun TestScreen() {
+    val totCardNumber by remember {
+        mutableStateOf(10)
+    }
+
+    val (curNum, setcurNum) = remember {
+        mutableStateOf(0)
+    }
+
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    LaunchedEffect(key1 = curNum) {
+        animationPlayed = true
+    }
+    // 안된다 ㅋㅋㅋㅋㅋ
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("${curNum + 1} / $totCardNumber")
+        QuizProgress(
+            modifier = Modifier
+                .fillMaxWidth(.8f)
+                .height(12.dp),
+            animationPlayed = animationPlayed,
+            curNum = curNum,
+            totNum = totCardNumber
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { setcurNum(curNum + 1) }) {
+            Text("다음 카드로 넘어가기")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Current value = $curNum")
+        if(curNum == totCardNumber - 1){
+            Text("끝")
+        }
+    }
+
+
+}
+
+@Composable
+fun QuizProgress(modifier: Modifier, animationPlayed: Boolean, curNum: Int, totNum: Int) {
+    val start = curNum / totNum.toFloat()
+    val end = (curNum + 1) / totNum.toFloat()
+    val curValue = animateFloatAsState(
+        targetValue = if (animationPlayed) end else start,
+        animationSpec = tween(
+            durationMillis = 500,
+            delayMillis = 0
+        )
+    )
+
+    Box(
+        modifier = modifier
+            .clip(CircleShape)
+            .background(Color.LightGray)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(curValue.value)
+                .background(color = DeepOrange)
+                .padding(8.dp)
+        ){}
+    }
+}
