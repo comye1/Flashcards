beed67acf4ae2b71d410884942548234be2aa650
comye1 <comye1@seoultech.ac.kr>
2021-10-12 13:45:57 +0900

    ProgressBar 구현

--- app/src/main/java/com/comye1/flashcards/MainActivity.kt
+++ app/src/main/java/com/comye1/flashcards/MainActivity.kt
@@ -25,10 +25,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.comye1.flashcards.navigation.BottomNavigationBar
 import com.comye1.flashcards.navigation.Screen
-import com.comye1.flashcards.screens.CreateScreen
-import com.comye1.flashcards.screens.DeckScreen
-import com.comye1.flashcards.screens.HomeScreen
-import com.comye1.flashcards.screens.MoreScreen
+import com.comye1.flashcards.screens.*
 import com.comye1.flashcards.ui.theme.FlashcardsTheme
 
 class MainActivity : ComponentActivity() {
@@ -66,8 +63,8 @@ class MainActivity : ComponentActivity() {
                         }
                         composable(Screen.More.route) {
                             setShowBottomBar(true)
-                            MoreScreen(navController)
-                            //                            TestScreen()
+//                            MoreScreen(navController)
+                            TestScreen()
                         }
 //                        composable("DeckScreen"){
 //                            DeckScreen(navController = navController)
--- app/src/main/java/com/comye1/flashcards/screens/TestScreen.kt
+++ app/src/main/java/com/comye1/flashcards/screens/TestScreen.kt
@@ -1,62 +1,67 @@
 package com.comye1.flashcards.screens
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.comye1.flashcards.ui.theme.DeepOrange
 
-// 전체
 @Preview
 @Composable
 fun TestScreen() {
-    val totCardNumber by remember {
-        mutableStateOf(10)
+    val (count, setCount) = remember { // 현재 카드 수
+        mutableStateOf(1f)
     }
-
-    val (curNum, setCurNum) = remember {
-        mutableStateOf(0)
-    }
-
-    var animationPlayed by remember {
-        mutableStateOf(false)
-    }
-    LaunchedEffect(key1 = curNum) {
-        animationPlayed = true
-    }
-    // 안된다 ㅋㅋㅋㅋㅋ
+    val totCount = 7 // 총 카드 수
     Column(
-        Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier.padding(16.dp)
     ) {
-        Text("${curNum + 1} / $totCardNumber")
-        QuizProgress(
-            modifier = Modifier
-                .fillMaxWidth(.8f)
-                .height(12.dp),
-            animationPlayed = animationPlayed,
-            curNum = curNum,
-            totNum = totCardNumber
+        //TopBar 들어갈 텍스트
+        Text(
+            text = " ${count.toInt()} / $totCount",
+            fontWeight = FontWeight.Bold
         )
+
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { setCurNum(curNum + 1) }) {
-            Text("다음 카드로 넘어가기")
-        }
+
+        // 프로그레스바
+        ProgressBar(count = count, totCount = totCount)
+
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Current value = $curNum")
-        if(curNum == totCardNumber - 1){
-            Text("끝")
+
+        // 카드 넘기는 동작 일단 버튼으로 구현
+        Row(
+            Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(
+                onClick = { if (count > 1) setCount(count - 1) }
+            ) {
+                Text("이전 카드")
+            }
+            Button(
+                onClick = { if (count < totCount) setCount(count + 1) }
+            ) {
+                Text("다음 카드")
+            }
         }
     }
 
@@ -64,28 +69,29 @@ fun TestScreen() {
 }
 
 @Composable
-fun QuizProgress(modifier: Modifier, animationPlayed: Boolean, curNum: Int, totNum: Int) {
-    val start = curNum / totNum.toFloat()
-    val end = (curNum + 1) / totNum.toFloat()
-    val curValue = animateFloatAsState(
-        targetValue = if (animationPlayed) end else start,
+fun ProgressBar(
+    modifier: Modifier = Modifier,
+    color: Color = DeepOrange,
+    animDuration: Int = 300,
+    animDelay: Int = 0,
+    count: Float,
+    totCount: Int,
+) {
+    val curPercentage by animateFloatAsState(
+        targetValue = count / totCount,
         animationSpec = tween(
-            durationMillis = 500,
-            delayMillis = 0
+            durationMillis = animDuration,
+            delayMillis = animDelay,
+            easing = LinearOutSlowInEasing
         )
     )
-
-    Box(
+    LinearProgressIndicator(
         modifier = modifier
-            .clip(CircleShape)
-            .background(Color.LightGray)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(curValue.value)
-                .background(color = DeepOrange)
-                .padding(8.dp)
-        ){}
-    }
+            .fillMaxWidth()
+            .height(12.dp)
+            .clip(CircleShape),
+        progress = curPercentage,
+        color = color,
+        backgroundColor = Color.LightGray
+    )
 }
